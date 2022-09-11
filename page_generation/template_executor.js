db76a184d08e7fe6e45a691d80ac89381a2241ec
Jinan-Dangor <andrew@trouty.com>
2022-09-11 16:58:44 +1000

    File and JSON reading & writing working

--- page_generation/template_executor.js
+++ page_generation/template_executor.js
@@ -1,7 +1,21 @@
 const fs = require("fs");
 
-fs.readFile("puzzle_page.txt", (err, data) => {
+var puzzle_data;
+data = fs.readFileSync("../data/puzzle_data.json", "utf8");
+const file_data = data.toString();
+puzzle_data = JSON.parse(file_data);
+console.log(puzzle_data.contents[0].ID);
+
+fs.readFile("puzzle_page.html", (err, data) => {
+  if (err) throw err;
+
+  var page_file = data.toString();
+
+  console.log(page_file);
+
+  fs.appendFile('puzzle_page_copy.html', page_file, function (err) {
     if (err) throw err;
-  
-    console.log(data.toString());
+
+    console.log('Saved!');
   });
+});
