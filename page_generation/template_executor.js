bf1664a09cacd90a2e9d2aa4f8e46c9e9c9797aa
Jinan-Dangor <andrew@trouty.com>
2022-09-11 18:53:29 +1000

    Template Executor working

--- page_generation/template_executor.js
+++ page_generation/template_executor.js
@@ -1,21 +1,50 @@
 const fs = require("fs");
 
 var puzzle_data;
-data = fs.readFileSync("../data/puzzle_data.json", "utf8");
-const file_data = data.toString();
-puzzle_data = JSON.parse(file_data);
-console.log(puzzle_data.contents[0].ID);
+{
+  data = fs.readFileSync("../data/puzzle_data.json", "utf8");
+  const file_data = data.toString();
+  puzzle_data = JSON.parse(file_data);
+}
 
-fs.readFile("puzzle_page.html", (err, data) => {
-  if (err) throw err;
+var content_page_html;
+{
+  data = fs.readFileSync("round_page_template.html", "utf8");
+  content_page_html = data.toString();
+}
 
-  var page_file = data.toString();
+var puzzle_page_html;
+{
+  data = fs.readFileSync("puzzle_page_template.html", "utf8");
+  puzzle_page_html = data.toString();
+}
 
-  console.log(page_file);
+for (let i = 0; i < puzzle_data.contents.length; i++) {
+  generate_page(puzzle_data.contents[i]);
+}
 
-  fs.appendFile('puzzle_page_copy.html', page_file, function (err) {
+function generate_page(item) {
+  if (item.class == "round") {
+    generate_round_page(item)
+  } else if (item.class == "puzzle") {
+    generate_puzzle_page(item)
+  } else {
+    throw {name : "JigsawClassNotFoundError", message : "The class" + item.class + " could not be found"};
+  }
+}
+
+function generate_round_page(round) {
+  fs.appendFile("round_page_" + round.page, puzzle_page_html, function (err) {
     if (err) throw err;
-
-    console.log('Saved!');
   });
-});
+
+  for (let i = 0; i < round.contents.length; i++) {
+    generate_page(round.contents[i]);
+  }
+}
+
+function generate_puzzle_page(puzzle) {
+  fs.appendFile("puzzle_page_" + puzzle.page, content_page_html, function (err) {
+    if (err) throw err;
+  });
+}
