bcabea8029b74886c76a34798a5f1f946fc0a676
Jinan-Dangor <andrew@trouty.com>
2022-09-18 16:08:15 +1000

    All Deployment Files now stored in hunt_deployment
    The init file is temporarily excluded since it needs to be in the home directory for testing, aim to make the outer init file just a blank landing page that sends you to the 'true' init page

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 Jigsaw Puzzle Hunt Constructor allows you to build static puzzle hunt websites that mimic the behaviour of live hunts.
 
 To make a hunt, fork this repository, then build up your hunt according to the documentation.
+Then copy the contents of the hunt_deployment folder into a new repo, and host it with github pages.
 
 Note: Do not apply attributes directly to jigsaw-element tags, these tags have attributes modified through code which may conflict with provided attributes.
 Instead, apply attributes to a div outside the tag, or otherwise.
--- page_generation/template_executor.js
+++ page_generation/template_executor.js
@@ -44,7 +44,7 @@ function generate_page(item, parents) {
 
 function generate_round_page(round, parents) {
   let round_page_html_modified = make_subtitutions(round_page_html, round, parents) + '\n\n<script src="template_filler.js"></script>';
-  fs.writeFileSync("round_page_" + round.page, round_page_html_modified);
+  fs.writeFileSync("../hunt_deployment/round_page_" + round.page, round_page_html_modified);
 
   for (let i = 0; i < round.contents.length; i++) {
     let new_parents = Array.from(parents);
@@ -55,7 +55,7 @@ function generate_round_page(round, parents) {
 
 function generate_puzzle_page(puzzle, parents) {
   let puzzle_page_html_modified = make_subtitutions(puzzle_page_html, puzzle, parents) + '\n\n<script src="template_filler.js"></script>';
-  fs.writeFileSync("puzzle_page_" + puzzle.page, puzzle_page_html_modified);
+  fs.writeFileSync("../hunt_deployment/puzzle_page_" + puzzle.page, puzzle_page_html_modified);
 }
 
 
