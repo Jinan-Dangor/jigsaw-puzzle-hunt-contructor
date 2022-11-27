3f0bcc56a70d85a2dcbcc0ca6afc0f09e2134b76
Jinan-Dangor <andrew@trouty.com>
2022-11-27 21:55:30 +1100

    Fixed waiting to start bug

--- hunt_deployment/scripts/waiting_to_start.js
+++ hunt_deployment/scripts/waiting_to_start.js
@@ -2,19 +2,29 @@ var puzzle_data = JSON.parse(localStorage.getItem("puzzle_data"));
 
 var waiting_to_start_timer = document.querySelectorAll("[jigsaw-element='waiting_to_start_timer']")[0];
 
+var timeUpdateIntervalID;
+
 if (puzzle_data != null && waiting_to_start_timer != null) {
-    setInterval(update_time, 1000);
+    
     let now = Date.now();
-    console.log(now);
     let start_time = new Date(puzzle_data.hunt_settings.start_time);
-    console.log(start_time);
     var time_left = Math.floor((start_time-now)/1000);
-    waiting_to_start_timer.innerHTML = `The hunt will start in ${seconds_to_nice_time(time_left)}.`;
+    if (time_left <= 0) {
+        waiting_to_start_timer.remove();
+    } else {
+        timeUpdateIntervalID = setInterval(update_time, 1000);
+        waiting_to_start_timer.innerHTML = `The hunt will start in ${seconds_to_nice_time(time_left)}.`;
+    }
 }
 
 function update_time() {
     time_left--;
-    waiting_to_start_timer.innerHTML = `The hunt will start in ${seconds_to_nice_time(time_left)}.`;
+    if (time_left <= 0) {
+        waiting_to_start_timer.innerHTML = `The hunt has started! Refresh the page!`;
+        clearInterval(timeUpdateIntervalID);
+    } else {
+        waiting_to_start_timer.innerHTML = `The hunt will start in ${seconds_to_nice_time(time_left)}.`;
+    }
 }
 
 // 'nice time' displays the two most sigificant numbers out of days, hours, minutes and seconds
